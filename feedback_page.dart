5cab1234ff30e601661fcd7d2d1bb8b0b00c49c8
MandyGaoGao <40432661+MandyGaoGao@users.noreply.github.com>
2019-04-22 04:50:31 +0800

    Create feedback_page.dart

---
+++ feedback_page.dart
@@ -0,0 +1,90 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+void main() => runApp(new MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      title: 'Feedback Page',
+      theme: new ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: new feedbackForm(title: 'Feedback Page'),
+    );
+  }
+}
+
+class feedbackForm extends StatefulWidget {
+  feedbackForm({Key key, this.title}) : super(key: key);
+  final String title;
+
+  @override
+  _feedbackFormState createState() => new _feedbackFormState();
+}
+
+class _feedbackFormState extends State<feedbackForm> {
+  final GlobalKey<FormState> _formKey = new GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text(widget.title),
+      ),
+      body: new SafeArea(
+          top: false,
+          bottom: false,
+          child: new Form(
+              key: _formKey,
+              autovalidate: true,
+              child: new ListView(
+                padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                children: <Widget>[
+                  new TextFormField(
+                    decoration: const InputDecoration(
+                      icon: const Icon(Icons.person),
+                      hintText: 'How you want to be addressed?',
+                      labelText: 'Name',
+                    ),
+                  ),
+                  new TextFormField(
+                    decoration: const InputDecoration(
+                      icon: const Icon(Icons.calendar_today),
+                      hintText: 'Any improvement or problems occurred?',
+                      labelText: 'Report Issue/Comment',
+                    ),
+                    keyboardType: TextInputType.datetime,
+                  ),
+                  new TextFormField(
+                    decoration: const InputDecoration(
+                      icon: const Icon(Icons.phone),
+                      hintText: 'How could we contact you?',
+                      labelText: 'Phone',
+                    ),
+                    keyboardType: TextInputType.phone,
+                    inputFormatters: [
+                      WhitelistingTextInputFormatter.digitsOnly,
+                    ],
+                  ),
+                  new TextFormField(
+                    decoration: const InputDecoration(
+                      icon: const Icon(Icons.email),
+                      hintText: 'How could we contact you?',
+                      labelText: 'Email',
+                    ),
+                    keyboardType: TextInputType.emailAddress,
+                  ),
+                 
+                  new Container(
+                      padding: const EdgeInsets.only(left: 40.0, top: 20.0),
+                      child: new RaisedButton(
+                        child: const Text('Submit'),
+                        onPressed: null,
+                      )),
+                ],
+              ))),
+    );
+  }
+}
